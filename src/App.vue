6dbb0840b05076d7c4b13ce1e318043325f1dc5b
treemmett <treemmett@gmail.com>
2018-07-24 16:39:18 -0600

    First!

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{message}}</h1>
+</template>
+
+<script>
+export default {
+  data: function(){
+    console.log('hello');
+    return {
+      message: 'Hello!'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  $red: rgb(194, 53, 53);
+
+  h1{
+    color: $red;
+    transform: translate(10px);
+  }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const config = require(__dirname + '/package');
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+
+const IS_DEV = path.basename(require.main.filename) === 'webpack-dev-server.js';
+
+module.exports = {
+  entry: [
+    './src/app.js'
+  ],
+
+  output: {
+    filename: 'main.[hash:6].js',
+    publicPath: IS_DEV ? '/' : config.root
+  },
+
+  devtool: IS_DEV ? 'source-map' : false,
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                require('autoprefixer')()
+              ]
+            }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin([IS_DEV ? '': 'dist'], {
+      root: __dirname
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+      minify: IS_DEV ? {} : {
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        removeComments: true,
+      }
+    }),
+
+    new VueLoaderPlugin(),
+  ]
+}
